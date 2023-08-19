c2ded1a079728f044c3c9396038347d787f5f3f4
Azka Zaki <azkazaki11@gmail.com>
2023-08-19 21:19:23 +0700

    Fix prisma sschema

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -9,21 +9,21 @@ const prisma = new PrismaClient();
 
 async function main() {
   // const hashPass = await bcrypt.hash('qwerty123', 10)
-  for (let staff of staffG) {
-    await prisma.staff_gudang.create({
-      data: staff,
-    });
-  }
-  for (let suppli of supplier) {
-    await prisma.supplier.create({
-      data: suppli,
-    });
-  }
-  for (let apotek of apoteker) {
-    await prisma.apoteker.create({
-      data: apotek,
-    });
-  }
+  // for (let staff of staffG) {
+  //   await prisma.staff_gudang.create({
+  //     data: staff,
+  //   });
+  // }
+  // for (let suppli of supplier) {
+  //   await prisma.supplier.create({
+  //     data: suppli,
+  //   });
+  // }
+  // for (let apotek of apoteker) {
+  //   await prisma.apoteker.create({
+  //     data: apotek,
+  //   });
+  // }
   for (let medicine of obat)
     [
       await prisma.obat.create({
--- backend/src/controller/PegawaiController.js
+++ backend/src/controller/PegawaiController.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
 
 const prisma = new PrismaClient();
 
@@ -50,6 +51,57 @@ export const getPegawai = async (req, res) => {
     }
 
     res.status(200).send(existingUser);
+  } catch (error) {
+    res.status(400).json({ message: error });
+    console.log(error);
+  }
+};
+const generateIdWithDate = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+  const idPembelian = `pbln-${formattedDate}-${uuidv4()}`;
+  return idPembelian;
+};
+export const createPembelian = async (req, res) => {
+  const { nama_obat, jenis_obat, tanggal_exp, jumlah, harga_beli, harga_jual, stok_obat, id_supplier, gambar, kategori_obat, sub_kategori, tipe_obat } = req.body;
+  //const id_staff = req.id_staff;
+  // const checkObat = await prisma.obat.findFirst({
+  //   where: { nama_obat: nama_obat },
+  // });
+  // if (checkObat) { return res.status(400).json({ msg: "Obat sudah ada" }); }
+  const idPembelian = generateIdWithDate();
+
+  const staff_id = req.userId;
+  console.log("Id staff: " + staff_id);
+
+  try {
+    const obat = await prisma.obat.create({
+      data: {
+        id: obatId,
+        nama_obat: nama_obat,
+        jenis_obat: jenis_obat,
+        tanggal_expired: tanggal_exp,
+        jumlah: jumlah,
+        harga_beli: harga_beli,
+        harga_jual: harga_jual,
+        stok_obat: stok_obat,
+        staff: {
+          connect: { id: staff_id },
+        },
+        supplier: {
+          connect: { id: id_supplier },
+        },
+        gambar: gambar,
+        kategori_obat: kategori_obat,
+        sub_kategori: sub_kategori,
+        tipe_obat: tipe_obat,
+      },
+    });
+    res.status(201).json(obat);
   } catch (error) {
     res.status(400).json({ message: error });
     console.log(error);
